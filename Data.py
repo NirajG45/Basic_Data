050efbf3934a5d242c4116b6ba53de013286c7ce
Niraj Kumar <nirajkumar9945111@gmail.com>
2025-06-11 00:09:00 +0530

    Updating my data_Visualization codes

--- Data.py
+++ Data.py
@@ -6,7 +6,15 @@ app = Flask(__name__)
 def index():
     data = {
         "labels": ["Python", "C++", "C", "MongoDB", "SQL", "MySQL"],
-        "values": [92, 85, 78, 80, 88, 90]
+        "values": [92, 85, 78, 80, 88, 90],
+        "colors": [
+            "#3572A5",  # Python - Blue
+            "#f34b7d",  # C++ - Pink
+            "#555555",  # C - Dark Gray
+            "#13AA52",  # MongoDB - Green
+            "#336791",  # SQL - Blue-Grey
+            "#00758F"   # MySQL - Teal Blue
+        ]
     }
     return render_template('index.html', data=data)
 
